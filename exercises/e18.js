a78cd52a6e6c6f2de668e0cf03a6901f3cdb3db5
freejah <Jt22980@gmail.com>
2023-12-15 16:01:05 -0500

    finished i think

--- exercises/e18.js
+++ exercises/e18.js
@@ -7,6 +7,32 @@
 export function getGreatestDiscoveryYear(data) {
   // Your code goes here...
   // feel free to import your `maxBy` or `minBy` methods from previous lessons
+  const asteroids = data.asteroids;
+  const discoveryYears = asteroids
+  .map(asteroid => asteroid.discoveryYear);
+
+  const count = discoveryYears
+  .reduce((a, num) => {
+    a[num] = (a[num] || 0) + 1;
+    return a;
+  }, {});
+
+  let maxOccurrences = -1;
+  let yearWithMostOccurrences = null;
+
+  // Iterate over the keys using a for...in loop
+  for (let num in count) {
+    // Ensure the key is not inherited from the prototype chain
+    if (count[num] !== undefined) {
+      const occurrences = count[num];
+      if (occurrences > maxOccurrences) {
+        maxOccurrences = occurrences;
+        yearWithMostOccurrences = Number(num);
+      }
+    }
+  }
+
+  return yearWithMostOccurrences;
 }
 
 // === TEST YOURSELF ===
